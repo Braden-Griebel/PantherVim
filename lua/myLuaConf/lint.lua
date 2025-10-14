94f8fdfbae82aa568e4fec93ca4b5e42ac27bbb9
Braden Griebel <bgriebel@uw.edu>
2025-10-14 12:45:04 -0700

    Updated README

--- lua/myLuaConf/lint.lua
+++ lua/myLuaConf/lint.lua
@@ -2,23 +2,14 @@ require("lze").load({
 	{
 		"nvim-lint",
 		for_cat = "languages",
-		-- cmd = { "" },
 		event = "FileType",
-		-- ft = "",
-		-- keys = "",
-		-- colorscheme = "",
 		after = function(plugin)
 			require("lint").linters_by_ft = {
-				-- NOTE: download some linters in lspsAndRuntimeDeps
-				-- and configure them here
-				-- markdown = {'vale',},
-				-- javascript = { 'eslint' },
-				-- typescript = { 'eslint' },
 				markdown = { "markdownlint-cli2" },
 				bash = { "shellcheck" },
 				python = { "mypy" },
 				javascript = { "oxlint", "eslint" },
-				typscript = { "esline" },
+				typscript = { "eslint" },
 			}
 
 			vim.api.nvim_create_autocmd({ "BufWritePost" }, {
